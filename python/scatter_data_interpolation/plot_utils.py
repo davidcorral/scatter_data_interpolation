8d3ea151bf7b9dd384e7e4c7ed5df1c85f008b73
David Corral <davidcorralsanmartin@gmail.com>
2022-01-06 16:09:35 -0800

    - added exponential function - added average sigma function - simplified plotting functions -

--- python/scatter_data_interpolation/plot_utils.py
+++ python/scatter_data_interpolation/plot_utils.py
@@ -1,11 +1,21 @@
+'''
+Refs:
+- https://scipython.com/book/chapter-7-matplotlib/examples/simple-surface-plots/
+
+'''
 import numpy
+from matplotlib import cm
 from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 
 from .radial_basis_function import rbf
+from .radial_basis_function import average_sigma
 from .radial_basis_function import solve_weights
 
+def pretty_name(snake_case):
+    return ' '.join([token.capitalize() for token in snake_case.split('_')])
 
-def plot_kernels(kernels):
+def plot_kernels(kernels, c):
     '''
     Plots Radial Basis Function Kernels.
     '''
@@ -16,16 +26,16 @@ def plot_kernels(kernels):
 
     # create plot figures
     f = pyplot.figure(figsize=(30, 30))
-    for i, (name, func, c) in enumerate(kernels):
-
+    for i, kernel_func in enumerate(kernels):
+        
         ax = f.add_subplot(n, n, i+1)
-        ax.title.set_text(name)
+        ax.title.set_text(pretty_name(kernel_func.__name__))
 
-        y = numpy.vectorize(func)(abs(x), c)
+        y = numpy.vectorize(kernel_func)(abs(x), c)
         ax.plot(x, y)
 
 
-def plot_1d(kernels, nodes, values):
+def plot_1d(kernels, nodes, values, figsize=(30, 30)):
     '''
     Plots 1D RBF
     '''
@@ -33,11 +43,13 @@ def plot_1d(kernels, nodes, values):
     n = len(kernels)
     x = numpy.linspace(0, 1, 100)[:, numpy.newaxis]
 
-    f = pyplot.figure(figsize=(30, 30))
-    for i, (name, kernel_func, c) in enumerate(kernels):
+    c = average_sigma(nodes, 0.75)
 
-        ax = f.add_subplot(n, n, i+1)
-        ax.title.set_text(name)
+    f = pyplot.figure(figsize=figsize)
+    for k, kernel_func in enumerate(kernels):
+
+        ax = f.add_subplot(n, n, k+1)
+        ax.title.set_text(pretty_name(kernel_func.__name__))
         if c is not None:
             ax.set_xlabel('Sigma: {}'.format(c))
 
@@ -52,8 +64,7 @@ def plot_1d(kernels, nodes, values):
         ax.scatter(nodes, values, s=100, c='red')
         ax.set_ybound(lower=0.0, upper=1)
 
-
-def plot_2d(kernels, nodes, values):
+def plot_2d(kernels, nodes, values, sigma=None, figsize=(30, 30)):
     '''
     Plots 2D RBF
     '''
@@ -62,18 +73,63 @@ def plot_2d(kernels, nodes, values):
     N = 100
     ti = numpy.linspace(0, 1, N)
 
+    if sigma is None:
+        sigma = average_sigma(nodes, 0.75)
+
     # create plot figures
     n = len(kernels)
-    f = pyplot.figure(figsize=(30, 30))
-    for i, (name, kernel_func, c) in enumerate(kernels):
+    f = pyplot.figure(figsize=figsize)
+    for k, kernel_func in enumerate(kernels):
 
-        ax = f.add_subplot(n, n, i+1)
-        ax.title.set_text(name)
-        if c is not None:
-            ax.set_xlabel('Sigma: {}'.format(c))
+        ax = f.add_subplot(n, n, k+1)
+        ax.title.set_text(pretty_name(kernel_func.__name__))
+        ax.set_xlabel('Sigma: {}'.format(sigma))
 
-        weights = solve_weights(nodes, values, kernel_function=kernel_func, sigma=c)
-        data = numpy.array([rbf([ti[j], ti[i]], nodes, weights, kernel_function=kernel_func, sigma=c) for i in range(N) for j in range(N)])
+        weights = solve_weights(nodes, values, kernel_function=kernel_func, sigma=sigma)
+        data = numpy.array([rbf([ti[j], ti[i]], nodes, weights, kernel_function=kernel_func, sigma=sigma) for i in range(N) for j in range(N)])
 
         ax.scatter(nodes[:, 0]*N, nodes[:, 1]*N, s=50, c=values, edgecolor='black')
         ax.imshow(data.reshape(N, N, 3), origin='lower')
+
+
+def plot_3d(kernels, nodes, values, sigma=None, figsize=(30, 30)):
+    
+    # Create grid
+    N = 100
+    x = numpy.linspace(0, 1, N)
+    y = x.copy()
+    X, Y = numpy.meshgrid(x, y)
+
+    if sigma is None:
+        sigma = average_sigma(nodes, 0.75)
+
+    # create plot figures
+    n = len(kernels)
+    f = pyplot.figure(figsize=figsize)
+    for k, kernel_func in enumerate(kernels):
+        
+        # ax = f.add_subplot(projection='3d')
+        # ax = f.gca(projection='3d')
+        ax = f.add_subplot(n, n, k+1, projection='3d') 
+        ax.title.set_text(pretty_name(kernel_func.__name__))
+        ax.set_xlabel('Sigma: {}'.format(sigma))
+
+        weights = solve_weights(nodes, values, kernel_function=kernel_func, sigma=sigma)
+        data = numpy.array([rbf([x[j], y[i]], nodes, weights, kernel_function=kernel_func, sigma=sigma) for i in range(N) for j in range(N)])
+
+        # Plot the surface.
+        # rstride=12, cstride=12, 
+        # c = cm.jet((values/numpy.amax(values)).ravel())
+        # surf = ax.plot_surface(X, Y, data.reshape(N, N), #cmap='jet', 
+        #                     linewidth=1, edgecolor='black', antialiased=True, zorder=0, alpha=0)
+        ax.plot_wireframe(X, Y, data.reshape(N, N),
+                          linewidth=1, edgecolor='black', antialiased=True, zorder=0)
+
+        # Plot sample points
+        ax.scatter(nodes[:, 0], nodes[:, 1], values, s=50, edgecolor='black', c='red', zorder=1)
+
+        # Customize the z axis.
+        ax.set_xlim(-0.01, 1.01)
+        ax.set_ylim(-0.01, 1.01)
+
+    pyplot.show()
--- python/scatter_data_interpolation/radial_basis_function.py
+++ python/scatter_data_interpolation/radial_basis_function.py
@@ -15,6 +15,10 @@ def linear(r, c):
     return 1.0-r/c if r < c else 0.0
 
 
+def exponential(r, c):
+    return exp(-r * (1.0/c**2))
+
+
 def gaussian(r, c):
     return exp(-(r**2/(2*c**2)))
 
@@ -40,29 +44,77 @@ def hardy_multiquadric(r, c):
     return sqrt(r**2 + c**2)
 
 
+KERNELS = [
+    linear,
+    exponential,
+    gaussian,
+    inverse_multiquadric,
+    thin_plate_spline,
+    multiquadric,
+    biharmonic,
+    hardy_multiquadric
+]
+
 # ==============================================================================
 #                                SOLVER
 # ==============================================================================
 
-def solve_weights(nodes, values, sigma=1.0,
-                  distance_function=euclidean, kernel_function=linear):
+def distance_matrix(nodes, distance_function=euclidean):
 
     n = len(nodes)
     A = numpy.eye(n)
 
     for i in range(n):
-        for j in range(n):
-            A[i, j] = kernel_function(distance_function(nodes[i], nodes[j]), sigma)
+        for j in range(i, n):
+            A[i, j] = A[j, i] = distance_function(nodes[i], nodes[j])
+
+    return A
+
+def average_sigma(nodes, weight=0.5):
+    '''
+    Computes averaged sigma based on closest and farthest nodes
+    '''
+
+    def upper_tri_indexing(A):
+        '''Returns upper triangle without diagonals'''
+        m = A.shape[0]
+        r,c = numpy.triu_indices(m, 1)
+        return A[r,c]
+    
+    weight = min(max(weight, 0.0), 1.0)
+    D = distance_matrix(nodes)
+    upper_triangle = upper_tri_indexing(D) # D[numpy.triu_indices_from(D)]
+
+    _min = numpy.min(upper_triangle)
+    _max = numpy.max(upper_triangle)
+    
+    return ((1-weight) * _min) + (weight * _max)
+
+def solve_weights(nodes, values, sigma=1.0, 
+                  distance_function=euclidean, kernel_function=linear):
+
+    # build A
+    D = distance_matrix(nodes, distance_function=distance_function)
+    A = numpy.vectorize(kernel_function)(D, sigma)
+    b = values
+
+    # Ax = b
+    # (A^-1)Ax = (A^-1)b
+    # x = (A^-1)b
+    Ai = numpy.linalg.inv(A)
+    w = Ai.dot(b)
 
     # solve weights
-    return numpy.linalg.solve(A, values)
+    # w = numpy.linalg.solve(A, values)
 
+    return w
 
 def rbf(current_node, nodes, weights, sigma=1.0,
         distance_function=euclidean, kernel_function=linear):
 
     n = len(nodes)
     a = numpy.array([kernel_function(distance_function(current_node, nodes[i]), sigma) for i in range(n)])
+    
     return numpy.dot(a, weights)
 
     # A = lil_matrix((n, n))
